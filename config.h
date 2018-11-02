3c4095d2711e8ccc5bf44f72fbf2fe6bb26846fa
schnedann <32240716+schnedann@users.noreply.github.com>
2018-11-02 23:29:55 +0100

    Messurements with some "Real World" Load
    Interessting the RaspberryPi with a almost virgin Raspian performs excelent, while the PC gets worse. not only the estimated Jitter gets larger but also the Histogram clearly indicates the difference between a embedded system and a ordinary PC

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ constexpr static uint8_t const WORK_SELECT    = 1;
 constexpr static long    const NSEC_PER_SEC   = 1000000000;
 constexpr static size_t  const MAX_SAFE_STACK = 8*1024;
 //                                            ssmmmuuunnn
-constexpr static size_t  const INTERVAL       =  25000000; //5ms
+constexpr static size_t  const INTERVAL       =   1000000; //5ms
 constexpr static double  const DINTERVAL      = static_cast<double>(INTERVAL)/static_cast<double>(NSEC_PER_SEC);
 
 #endif // CONFIG_H
